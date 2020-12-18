4046ca0c57ff1d16d3cfdd6ffb3dd34eda868c95
ISMAKOVA <48486359+ISMAKOVA@users.noreply.github.com>
2020-12-19 01:22:39 +0500

    вывод registryItems

--- ANIMAUX/ANIMAUX/Controllers/HomeController.cs
+++ ANIMAUX/ANIMAUX/Controllers/HomeController.cs
@@ -18,7 +18,19 @@ namespace ANIMAUX.Controllers
 
         public ActionResult Registry()
         {
-            return View(entities.animals.ToList());
+            List<cards> cardsList = entities.cards.ToList();
+            List<animals> animalsList = entities.animals.ToList();
+            List<districts> districtsList = entities.districts.ToList();
+            ViewData["registryItems"] = from c in cardsList
+                                        join a in animalsList on c.animal_id equals a.passport_number 
+                                        into table1
+                                        from a in table1.DefaultIfEmpty()
+                                        join d in districtsList on c.district_id equals d.id
+                                        into table2
+                                        from d in table2.DefaultIfEmpty()
+                                        select new RegistryItems { cards = c, animals = a, districts = d };
+
+            return View(ViewData["registryItems"]);
         }
 
         public ActionResult Publications()
---
+++ ANIMAUX/ANIMAUX/Models/RegistryItems.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ANIMAUX.Models
+{
+    public class RegistryItems
+    {
+        public cards cards { get; set; }
+        public animals animals { get; set; }
+        public districts districts { get; set; }
+
+    }
+}
