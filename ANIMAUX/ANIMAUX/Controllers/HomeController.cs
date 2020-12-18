34141a90dee8e162e0c761be7cefc5114a378748
Mita57 <57thrasher57@gmail.com>
2020-12-18 21:32:59 +0500

    Added entity implementation

--- ANIMAUX/ANIMAUX/Controllers/HomeController.cs
+++ ANIMAUX/ANIMAUX/Controllers/HomeController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ANIMAUX.Models;
 
 namespace ANIMAUX.Controllers
 {
     public class HomeController : Controller
     {
+        AnimauxEntities entities = new AnimauxEntities();
+
         public ActionResult Profile()
         {
             return View();
